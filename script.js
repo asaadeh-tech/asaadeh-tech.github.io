048913773f25d4e9b70cc15492d4d53f0b53f763
Amir S <asaadeh98@gmail.com>
2021-03-03 19:21:03 -0600

    finish homepage
    adds most of the assets

--- script.js
+++ script.js
@@ -6,8 +6,6 @@ document.addEventListener('DOMContentLoaded', function() {
     };
     var instances = M.Carousel.init(elems, options);
 
-    //var sliderElem = document.querySelector('.carousel-slider');
-    //var instance = M.Carousel.getInstance(sliderElem);
     instances.forEach(instance => {
         setInterval(function () {
             instance.next();
